194a0f0edd6b0f38d51e49bc727e58b9cb546093
SharlottaL <botanichkagigantika@gmail.com>
2026-07-22 16:15:07 +0300

    TODO Teachers

--- 07-OOP/inc/main.php
+++ 07-OOP/inc/main.php
@@ -1,9 +1,30 @@
 <?php
 require_once __DIR__ . '/point.php';
+require_once __DIR__ . '/inheritance.php';
 
-$A = new Point(2,3);
-$A->info();
-$A->x = 7;
-$A->y = 8;
-echo $A;
+//$A = new Point(2,3);
+//$A->info();
+//$A->x = 7;
+//$A->y = 8;
+//echo $A;
+
+$human = new Human("Verchetty", "Tommy", 30);
+echo $human . '<br>';
+//$t_student = new Student($human, "");
+
+$student = new Student("Pinkman", "Jessie", 20, "Chemistry", "WW_221", 90,95);
+echo $student . '<br>';
+
+$graduate = new Graduate
+(
+    "Schreder",
+    "Hank",
+    40,
+    "Criminalistic",
+    "OBN",
+    50,
+    70,
+    "How to catch Hiesenberg"
+);
+echo $graduate . '<br>';
 ?>
